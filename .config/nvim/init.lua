111b68480a0039a7ca81d8888fb6c43050d1d26f
Thorben Westerhuys <twesterhuys@esri.com>
2024-10-26 17:12:29 +0200

    migrate to neovim lua config

---
+++ .config/nvim/init.lua
@@ -0,0 +1,33 @@
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
+end
+vim.opt.rtp:prepend(lazypath)
+
+vim.opt.number = "relativenumber"
+vim.opt.autoindent = true
+vim.opt.smartindent = true
+vim.opt.expandtab = true
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.noerrorbells = true
+vim.opt.visualbell = true
+
+-- Setup lazy.nvim
+require("lazy").setup({
+  spec = { 'ayu-theme/ayu-vim' },
+  install = { colorscheme = { "ayu" } },
+  -- automatically check for plugin updates
+  checker = { enabled = true },
+})
